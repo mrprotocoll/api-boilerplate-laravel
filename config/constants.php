6c960dcfc73c204a0f4caae25a7d68bb3db2918c
Damilare Protocoll <lekanvgbg@gmail.com>
2024-12-15 16:00:08 +0100

    Update constants.php

--- config/constants.php
+++ config/constants.php
@@ -5,6 +5,7 @@ return [
     // Pagination number
     'per_page' => 10,
     'user_dashboard' => $application.'/dashboard',
+    'reset_password' => $application.'/reset_password?t=',
     'email_verification_link' => '/auth/login'
 ];
 
